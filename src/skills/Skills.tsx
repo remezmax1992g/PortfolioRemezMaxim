c72938d2dcbe2e8d3607671f1282cd26679a09c0
Maxim Remez <remezmaxim1992@gmail.com>
2022-09-18 10:28:57 +0300

    create mixins, percentage color, add icon and style for skill

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -3,17 +3,63 @@ import style from "./Skills.module.scss"
 import styleContainer from "../Common/styles/Container.module.css"
 import Skill from "./skill/Skill";
 import Title from "../Common/components/title/Title";
+import htmlImg from "../Common/assets/images/html.png"
+import cssImg from "../Common/assets/images/css.png"
+import jsImg from "../Common/assets/images/js.png"
+import reactImg from "../Common/assets/images/react.png"
+import reduxImg from "../Common/assets/images/redux.png"
+import storybookImg from "../Common/assets/images/storybook.png"
+import materialMuiImg from "../Common/assets/images/MaterialMui.png"
+import restApiImg from "../Common/assets/images/rest-api.png"
+import gitImg from "../Common/assets/images/git.png"
+import unitTestImg from "../Common/assets/images/unit-test.png"
 
 const Skills = () => {
+    const htmlURL = {
+        backgroundImage: `url(${htmlImg})`
+    }
+    const cssURL = {
+        backgroundImage: `url(${cssImg})`
+    }
+    const jsURL = {
+        backgroundImage: `url(${jsImg})`
+    }
+    const reactURL = {
+        backgroundImage: `url(${reactImg})`
+    }
+    const reduxURL = {
+        backgroundImage: `url(${reduxImg})`
+    }
+    const storybookURL = {
+        backgroundImage: `url(${storybookImg})`
+    }
+    const materialMuiURL = {
+        backgroundImage: `url(${materialMuiImg})`
+    }
+    const restApiURL = {
+        backgroundImage: `url(${restApiImg})`
+    }
+    const gitURL = {
+        backgroundImage: `url(${gitImg})`
+    }
+    const unitTestURL = {
+        backgroundImage: `url(${unitTestImg})`
+    }
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} + ${style.skillsContainer}`}>
                 <Title titleName="My skills"/>
                 <div className={style.skills}>
-                    <Skill title="HTML" description="888888888888"/>
-                    <Skill title="CSS" description="7777777777777"/>
-                    <Skill title="JS" description="4444444444444"/>
-                    <Skill title="React" description="111111111"/>
+                    <Skill style={htmlURL} title="HTML" percentage={70}/>
+                    <Skill style={cssURL} title="CSS/SASS" percentage={70}/>
+                    <Skill style={jsURL} title="JS/TS" percentage={75}/>
+                    <Skill style={reactURL} title="React" percentage={85}/>
+                    <Skill style={reduxURL} title="Redux" percentage={80}/>
+                    <Skill style={storybookURL} title="Storybook" percentage={50}/>
+                    <Skill style={materialMuiURL} title="Material MUI" percentage={60}/>
+                    <Skill style={restApiURL} title="Rest-API" percentage={55}/>
+                    <Skill style={gitURL} title="Git" percentage={50}/>
+                    <Skill style={unitTestURL} title="Unit test" percentage={75}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
 import style from "./Skill.module.scss"
 
 type SkillType = {
     title: string
-    description: string
+    percentage: number
+    style:{
+        backgroundImage: string
+    }
 }
 
 const Skill = (props: SkillType) => {
+
     return (
         <div className={style.skill}>
             <div className={style.imgSkill}>
-                <div className={style.icon}>
-
-                </div>
+                <div className={style.icon} style={props.style}></div>
             </div>
-            <h3 className={style.title}>{props.title}</h3>
-            <span className={style.description}>{props.description}</span>
+            <div className={style.skillInfo}>
+                <div className={style.circle}>
+                    <div className={style.outer}>
+                        <div className={style.inner}>
+                            <div id="number" className={style.number}>{props.percentage + "%"}</div>
+                        </div>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+                        <defs>
+                            <linearGradient id="GradientColor">
+                                <stop offset="0%" stop-color="#e91e63" />
+                                <stop offset="100%" stop-color="#673ab7" />
+                            </linearGradient>
+                        </defs>
+                        <circle cx="35" cy="35" r="30" stroke-linecap="round" style={{strokeDashoffset: (185 - 185*props.percentage/100) }}/>
+                    </svg>
+                </div>
+
+                <h3 className={style.title}>{props.title}</h3>
+
+            </div>
         </div>
     );
 };
