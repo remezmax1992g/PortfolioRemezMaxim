fb482579924c1e4f298ff4a3c5593baa95d15662
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-15 01:09:54 +0300

    modification of main component

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import style from "./Main.module.scss"
-import styleContainer from "../Common/styles/Container.module.css"
 import photo from "../Common/assets/images/myphoto.jpg"
 import {Button} from "@mui/material";
 
 const Main = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={styleContainer.container}>
-                <div className={style.textAreaBlock}>
-                    <div className={style.textArea}>
-                        <div className={style.greetingTextBlock}>
+            <div className={style.container}>
+                <div className={style.personalInformation}>
+                    <div className={style.textAreaBlock}>
+                        <div className={style.textArea}>
                             <span className={style.greetingText}>Hi there</span>
-                        </div>
-                        <div className={style.nameTextBlock}>
                             <h1 className={style.nameText}>I'm Maxim Remez</h1>
-                        </div>
-                        <div className={style.professionTextBlock}>
                             <p className={style.professionText}>Frontend developer React</p>
                         </div>
+                        <div className={style.buttons}>
+                            <Button variant="contained" style={{width: "160px", borderRadius: "20px"}}>MORE ABOUT
+                                ME</Button>
+                            <Button variant="contained" component="label"
+                                    style={{width: "160px", borderRadius: "20px"}}>
+                                upload CV
+                                <input hidden accept="image/*" multiple type="file"/>
+                            </Button>
+                        </div>
                     </div>
-                    <div className={style.buttonAboutMe}>
-                        <Button variant="contained" color="secondary">MORE ABOUT ME</Button>
+                    <div className={style.mainPhoto}>
+                        <img className={style.myPhoto} src={photo} alt="no photo"/>
                     </div>
-                </div>
-                <div className={style.mainPhoto}>
-                    <img className={style.myPhoto} src={photo} alt="no photo"/>
                 </div>
             </div>
         </div>
