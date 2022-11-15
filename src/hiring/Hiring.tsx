b2f7283c72e5900a257d3d7c734ff9a001004a0c
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-15 04:24:19 +0300

    modification of Hiring

--- src/hiring/Hiring.tsx
+++ src/hiring/Hiring.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import style from "./Hiring.module.scss"
-import styleContainer from "../Common/styles/Container.module.css"
 import Title from "../Common/components/title/Title";
 import {Button} from "@mui/material";
 
 const Hiring = () => {
     return (
         <div className={style.hiringBlock}>
-            <div className={`${styleContainer.container} + ${style.hiringContainer}`}>
+            <div className={style.container}>
                 <Title titleName="Considering remote work options"/>
-                <Button variant="contained" color="success">Hire me</Button>
+                <Button className={style.button} variant="contained" >Hire me</Button>
             </div>
 
         </div>
